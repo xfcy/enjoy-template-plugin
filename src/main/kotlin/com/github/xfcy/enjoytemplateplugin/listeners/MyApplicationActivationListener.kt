a3823a1c9862c12ac12704296b233b7e584cfc49
GitHub Action <action@github.com>
2023-07-29 03:56:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/xfcy/enjoytemplateplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/xfcy/enjoytemplateplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.xfcy.enjoytemplateplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/xfcy/enjoytemplateplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/xfcy/enjoytemplateplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.xfcy.enjoytemplateplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xfcy.enjoytemplateplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
